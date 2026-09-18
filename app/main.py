5acafc9f9dcccf6a90d87cc2ee83021543d9cd71
Jean Noël <jeannoelp25sesame@gmail.com>
2026-09-18 19:42:47 +0300

    configuration .env

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ settings=get_settings()
 
 app=FastAPI(
     title="VADY-LUX APIS",
-    docs_url=f"{settings.prefix_app}/docs" if settings.environment=="development" else None,
+    docs_url=f"{settings.prefix_app}/docs",
     redoc_url=f"{settings.prefix_app}/redoc" if settings.environment=="development" else None
 )
 
